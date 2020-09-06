31c44761725ac1042aa327243090a9ce38e3b78e
SloshyMarcos111 <58153869+SloshyMarcos111@users.noreply.github.com>
2020-09-06 16:44:25 -0400

    made print_function and added comments to code print_function prints the decomposed address lists in two sections

--- regex.py
+++ regex.py
@@ -2,14 +2,39 @@
 # -*- coding: utf-8 -*-
 import re
 
-f = open("addresses.csv")
-fixed = []
 
-for address in f:
-    match = re.split('(\d.+)', address)
-    fixed.append(match)
+"""
+function that takes in two section of the split address and prints each section with headings
+@:param areas is a list of the first section of the address
+@:param blocks is a list of the second section of the address
+"""
+def print_function(areas, blocks):
+    print("------------------------- area section ---------------------------")
 
+    for element in areas:
+        print element
+
+    print("------------------------- block section ---------------------------")
+
+    for element in blocks:
+        print element
+
+# opening file that contains addresses with read only access
+f = open("addresses.csv", "r")
+fixed = []      # list of split addresses
+areas = []      # first section of addresses (areas)
+blocks = []     # second section of addresses (blocks)
+
+# parsing file
+for line in f:
+    match = re.split('(\d.+)', line)    # splits entry in address file by the first digit encountered
+    fixed.append(match)                 # appends the decomposed address into the split addresses list
+
+f.close()   # close file
+
+# appends the areas and blocks sections into respective lists
 for list in fixed:
-    for string in list:
-        print string
+    areas.append(list[0])
+    blocks.append(list[1])
 
+print_function(areas, blocks)   # call for printing the lists
