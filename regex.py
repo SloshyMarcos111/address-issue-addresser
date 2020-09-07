2a0521e017ce6daf86f411b41f7c701a7a3021eb
SloshyMarcos111 <58153869+SloshyMarcos111@users.noreply.github.com>
2020-09-06 20:33:16 -0400

    added the combine function that recomposes the addresses with the correct formatting this is all just experimental and preliminary and a lot of it is just me getting the gist of the scope of the program

--- regex.py
+++ regex.py
@@ -2,22 +2,38 @@
 # -*- coding: utf-8 -*-
 import re
 
+
+"""
+function that takes an element from the list of areas and appends the corresponding numbers in the correct format
+@:param first an element from the list of areas from the decomposed addresses
+@:param is a list of numbers element from the block numbers list of the addresses
+@:return the combined address with the correct formatting
+"""
+# this is where the addresses will be reformatted
+def combine(first, numbers_list):
+    combined = first + numbers_list[0] + "-" + numbers_list[1] + "-" + numbers_list[2]
+    return combined
+
+
 """
 function that takes the blocks list and decomposes to the numbers in it
 @:param blocks is a list of the second part of the addresses
 """
-def get_numbers(blocks):
-
-    address_num = []    # list of numbers in the address block
+def get_numbers(blocks_list):
+    address_num = []  # list of numbers in the address block
 
     # iterate through the blocks and append the numbers to the address_num list
-    for block in blocks:
-        numbers = re.findall(r'\d+', block)
-        address_num.append(numbers)
+    for block in blocks_list:
+        numbers_in_blocks = re.findall(r'\d+', block)
+        address_num.append(numbers_in_blocks)
 
     # print the numbers
+    print("\n\n------------------------- block numbers ---------------------------")
+
     for number in address_num:
         print(number)
+
+    return address_num
 
 
 """
@@ -25,37 +41,48 @@ function that takes in two section of the split address and prints each section 
 @:param areas is a list of the first section of the address
 @:param blocks is a list of the second section of the address
 """
-def print_function(areas, blocks):
-    print("------------------------- area section ---------------------------")
 
-    for element in areas:
+
+def print_function(area_list, block_list):
+    print("\n\n------------------------- area section ---------------------------")
+
+    for element in area_list:
         print element
 
-    print("------------------------- block section ---------------------------")
+    print("\n\n------------------------- block section ---------------------------")
 
-    for element in blocks:
+    for element in block_list:
         print element
 
 
 # opening file that contains addresses with read only access
 f = open("addresses.csv", "r")
-fixed = []      # list of split addresses
-areas = []      # first section of addresses (areas)
-blocks = []     # second section of addresses (blocks)
+fixed = []  # list of split addresses
+areas = []  # first section of addresses (areas)
+blocks = []  # second section of addresses (blocks)
 
 # parsing file
+print("\n\n------------------------- original addresses ---------------------------")
 for line in f:
-    match = re.split('(\d.+)', line)    # splits entry in address file by the first digit encountered
-    fixed.append(match)                 # appends the decomposed address into the split addresses list
+    print(line)
+    match = re.split('(\d.+)', line)  # splits entry in address file by the first digit encountered
+    fixed.append(match)  # appends the decomposed address into the split addresses list
 
-f.close()   # close file
+f.close()  # close file
 
 # appends the areas and blocks sections into respective lists
 for subs in fixed:
     areas.append(subs[0])
     blocks.append(subs[1])
 
-print_function(areas, blocks)   # call for printing the lists
-get_numbers(blocks)             # get the numbers in the block list
+print_function(areas, blocks)  # call for printing the lists
+numbers = get_numbers(blocks)  # get the numbers in the block list
 
+combined_addresses = []
+for i in range(len(areas)):
+    combined_addresses.append(combine(areas[i], numbers[i]))
+
+print("\n\n------------------------- reformatted address ---------------------------")
+for element in combined_addresses:
+    print(element)
 
